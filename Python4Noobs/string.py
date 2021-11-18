c7cae9c6363f92085cbe7b70526776412358fb45
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2021-11-18 20:24:19 -0300

    first commit

---
+++ Python4Noobs/string.py
@@ -0,0 +1,6 @@
+string1 = 'Olá mundo'
+string2 = "H4rtDevs é maior grupo de devs do Brasil"
+string3 = """Nossa, bem grande essa string..."""
+print(type(string1))
+print(type(string2))
+print(type(string3))
